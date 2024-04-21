b36c1b210716f33b5fff8240c0ba519ca587d078
Ambika Rama Katyayini <35074062+AmbikaKachetti@users.noreply.github.com>
2024-04-21 15:55:34 +0530

    Add files via upload

--- style.js
+++ style.js
@@ -1,1 +1,22 @@
-let userElement = document.getAnimations("")
+const toggleBtn = document.getElementById('toggle')
+
+const containerElement = document.getElementById('container')
+
+const homeScreenLeftElement = document.getElementById('home-screen-left')
+
+const homeScreenRightElement = document.getElementById('home-screen-right')
+
+
+function DarkLightMode(){
+    toggleBtn.classList.toggle('toggle--light')
+    toggleBtn.classList.toggle('toggle--dark')
+
+    containerElement.classList.toggle('--container--light')
+    containerElement.classList.toggle('--container--dark')
+
+    homeScreenLeftElement.classList.toggle('--container--light')
+    homeScreenLeftElement.classList.toggle('--container--dark')
+
+    homeScreenRightElement.classList.toggle('--container--light')
+    homeScreenRightElement.classList.toggle('--container--dark')
+}
